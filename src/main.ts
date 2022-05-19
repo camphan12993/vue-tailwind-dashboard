4bde824ccd5bb8746f823c7373d0a617b2cb909f
Cam Phan <cam.phan12993@gmail.com>
2022-05-19 23:17:58 +0700

    remove source

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,9 @@ import router from './router';
 import store from './store';
 import './assets/tailwind.css';
 import 'boxicons/css/boxicons.min.css';
-import { applyPolyfills, defineCustomElements } from '@aws-amplify/ui-components/loader';
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
 Amplify.configure(awsconfig);
-
-applyPolyfills().then(() => {
-  defineCustomElements(window);
-});
 
 const app = createApp(App);
 app.use(store).use(router).mount('#app');
